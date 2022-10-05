b82a1496a433e38d207b46a11d437e0cee5332c8
Eric Coimbra <eric.coimbra@gmail.com>
2022-10-05 11:32:27 +0100

    Iteration2 done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
 // Iteration 1: Names and Input
 //
-// Iteration 2: Conditionals
+console.log("I'm ready")
 
+
+// Iteration 1: Names and Input
+//
+const hacker1 = "Eric";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Giulia";
+console.log(`The navigator's name is ${hacker2}`);
+
+// Iteration 2: Conditionals
+function nameSize (driver, navigator) {
+    const size1 = driver.length;
+    const size2 = navigator.length;
+    if (size1>size2) {
+        console.log (`The driver has the longest name, it has ${size1} characters.`)
+    } else if (size2>size1) {
+        console.log (`It seems that the navigator has the longest name, it has ${size2} characters.`);
+    } else if (size1===size2) {
+        console.log (`Wow, you both have equally long names, ${size1} characters!`);
+    } else {
+        console.log ("Something went wrong!");
+    }
+}
+
+nameSize(hacker1, hacker2);
 
 // Iteration 3: Loops
